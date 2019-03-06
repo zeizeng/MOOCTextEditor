60bf02b9c5c08a8977dcd001d99844f81bae2933
NeilZeng <1986605791@qq.com>
2019-03-06 15:53:47 +0800

    week2

--- src/main/java/neil/personal/document/Document.java
+++ src/main/java/neil/personal/document/Document.java
@@ -15,20 +15,24 @@ public abstract class Document {
 
     private String text;
 
-    /** Create a new document from the given text.
+    /**
+     * Create a new document from the given text.
      * Because this class is abstract, this is used only from subclasses.
+     *
      * @param text The text of the document.
      */
     protected Document(String text) {
         this.text = text;
     }
 
-    /** Returns the tokens that match the regex pattern from the document
+    /**
+     * Returns the tokens that match the regex pattern from the document
      * text string.
+     *
      * @param pattern A regular expression string specifying the
-     *   token pattern desired
+     * token pattern desired
      * @return A List of tokens from the document text that match the regex
-     *   pattern
+     * pattern
      */
     protected List<String> getTokens(String pattern) {
         ArrayList<String> tokens = new ArrayList<String>();
@@ -42,7 +46,8 @@ public abstract class Document {
         return tokens;
     }
 
-    /** This is a helper function that returns the number of syllables
+    /**
+     * This is a helper function that returns the number of syllables
      * in a word.  You should write this and use it in your
      * BasicDocument class.
      *
@@ -55,21 +60,35 @@ public abstract class Document {
      * characters in the string and write your own logic for counting
      * syllables.
      *
-     * @param word  The word to count the syllables in
+     * @param word The word to count the syllables in
      * @return The number of syllables in the given word, according to
      * this rule: Each contiguous sequence of one or more vowels is a syllable,
-     *       with the following exception: a lone "e" at the end of a word
-     *       is not considered a syllable unless the word has no other syllables.
-     *       You should consider y a vowel.
+     * with the following exception: a lone "e" at the end of a word
+     * is not considered a syllable unless the word has no other syllables.
+     * You should consider y a vowel.
      */
     protected int countSyllables(String word) {
         // TODO: Implement this method so that you can call it from the
         // getNumSyllables method in BasicDocument (module 2) and
         // EfficientDocument (module 3).
-        return 0;
+        Pattern pattern = Pattern.compile("[AEIOUaeiouy]+");
+        Matcher matcher = pattern.matcher(word);
+        int count = 0;
+        int endE = 0;
+        while (matcher.find()) {
+            String group = matcher.group();
+
+            if (group.matches("^(e|E)+$") && matcher.start() + group.length() == word.length()) {
+                endE++;
+            } else {
+                count++;
+            }
+        }
+        return count == 0 ? endE : count;
     }
 
-    /** A method for testing
+    /**
+     * A method for testing
      *
      * @param doc The Document object to test
      * @param syllables The expected number of syllables
@@ -109,25 +128,35 @@ public abstract class Document {
     }
 
 
-    /** Return the number of words in this document */
+    /**
+     * Return the number of words in this document
+     */
     public abstract int getNumWords();
 
-    /** Return the number of sentences in this document */
+    /**
+     * Return the number of sentences in this document
+     */
     public abstract int getNumSentences();
 
-    /** Return the number of syllables in this document */
+    /**
+     * Return the number of syllables in this document
+     */
     public abstract int getNumSyllables();
 
-    /** Return the entire text of this document */
+    /**
+     * Return the entire text of this document
+     */
     public String getText() {
         return this.text;
     }
 
-    /** return the Flesch readability score of this document */
+    /**
+     * return the Flesch readability score of this document
+     */
     public double getFleschScore() {
         // TODO: You will play with this method in week 1, and
         // then implement it in week 2
-        return this.text.length();
+        return 206.835 - 1.015 * ((float) getNumWords() / (float) getNumSentences()) - 84.6 * ((float) getNumSyllables() / (float) getNumWords());
     }
 
 
--- src/test/java/neil/personal/test.java
+++ src/test/java/neil/personal/test.java
@@ -1,5 +1,8 @@
 package neil.personal;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Description
  *
