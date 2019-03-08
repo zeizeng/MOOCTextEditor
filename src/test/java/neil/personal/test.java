57c6ccfdccfce7d3e1f5b6c679fe8f3cf2bbe3f8
NeilZeng <1986605791@qq.com>
2019-03-08 16:37:29 +0800

    week3

--- src/test/java/neil/personal/test.java
+++ src/test/java/neil/personal/test.java
@@ -12,6 +12,17 @@ import java.util.regex.Pattern;
  */
 public class test {
     public static void main(String[] args) {
+        String a = "Here is a series of test sentences. Your program should find 3 sentences, 33 words, and 49 syllables. Not every word will have the correct amount of syllables (example, for example), but most of them will.";
+        String[] array = a.split("[. 0-9,()]+");
+        Pattern pattern = Pattern.compile("[AIOUYaiouy]*[eE]+[AIOUYaiouy]+[eE]*|[AIOUYaiouy]+|[AIOUYaiouy]*[eE]+\\B");
+        for (String s : array) {
+            Matcher matcher = pattern.matcher(s);
+            System.out.print(s + "\t");
+            while (matcher.find()) {
+                System.out.print(matcher.group() + "\t");
+            }
+            System.out.println();
+        }
 
     }
 }
